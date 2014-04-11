79dac5017dd3ecdd4f018a8c88e43ac91f034aa9
Reuben Crimp <reubencrimp@gmail.com>
2014-04-11 12:35:16 +1200

    compiled build

--- source/vector_ops.c
+++ source/vector_ops.c
@@ -78,10 +78,22 @@ Vector vector_normalise(Vector a){
 }
 Vector vector_transform(Matrix m, Vector v){
    Vector result;
+
+   Vector r1 = vector_new(m.element[0][0], m.element[0][1], m.element[0][2], m.element[0][3]);
+   Vector r2 = vector_new(m.element[1][0], m.element[1][1], m.element[1][2], m.element[1][3]);
+   Vector r3 = vector_new(m.element[2][0], m.element[2][1], m.element[2][2], m.element[2][3]);
+   Vector r4 = vector_new(m.element[3][0], m.element[3][1], m.element[3][2], m.element[3][3]);
+
+   result.x = vector_dot(r1, v);
+   result.y = vector_dot(r2, v);
+   result.z = vector_dot(r3, v);
+   result.y = vector_dot(r4, v);
+   
+   /*
    result.x = m.element[0][0]*v.x + m.element[0][1]*v.y + m.element[0][2]*v.z + m.element[0][3]*v.w;
    result.y = m.element[1][0]*v.x + m.element[1][1]*v.y + m.element[1][2]*v.z + m.element[1][3]*v.w;
    result.z = m.element[2][0]*v.x + m.element[2][1]*v.y + m.element[2][2]*v.z + m.element[2][3]*v.w;
-   result.w = m.element[3][0]*v.x + m.element[3][1]*v.y + m.element[3][2]*v.z + m.element[3][3]*v.w;
+   result.w = m.element[3][0]*v.x + m.element[3][1]*v.y + m.element[3][2]*v.z + m.element[3][3]*v.w;*/
    return result;
 }
 
