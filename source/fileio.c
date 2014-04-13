ae71226fadd5e036003006264395aec57c481a09
Reuben Crimp <reubencrimp@gmail.com>
2014-04-14 00:20:18 +1200

    compiled build

--- source/fileio.c
+++ source/fileio.c
@@ -269,9 +269,9 @@ void fileio_readfile(char *fname) {
          fscanf(description_file, "%lf", &z);
 
          matrix_make(&transformation,
-                     x, 0.0, 0.0, 0.0,
-                     0.0, y, 0.0, 0.0,
-                     0.0, 0.0, z, 0.0,
+                     1/x, 0.0, 0.0, 0.0,
+                     0.0, 1/y, 0.0, 0.0,
+                     0.0, 0.0, 1/z, 0.0,
                      0.0, 0.0, 0.0, 1.0);
          /* must stretch either the camera or most recent sphere */
          if(num_objs == 0){
