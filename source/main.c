215e697d527d6b4ba8bb19ad1c6844f01bed694f
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 04:33:02 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -102,8 +102,7 @@ int shadow_ray(Vector point1, Vector point2, int obj_caster) {
    closest_obj = -1;
    
    for(cur_obj = 0; cur_obj < num_objs; cur_obj++){
-      //if (cur_obj == obj_caster) break; /* assume no self shadowing */
-      
+      if (cur_obj == obj_caster) break; /* assume no self shadowing */
       cur_ray.start = vector_transform(ray.start, object[cur_obj].transform);
       cur_ray.direction = vector_transform(ray.direction, object[cur_obj].transform);
       temp = vector_length(cur_ray.direction);
@@ -221,11 +220,12 @@ RGBColour ray_trace(RayDef ray, int recurse_depth) {
          cur_light_pos = vector_transform(light_source[cur_light].position, object[closest_obj].transform);
 
          Vector rename_me =
-            vector_new(
+            vector_transform(SurfaceNormal, object[closest_obj].transform);
+            /*vector_new(
                        SurfaceNormal.x += object[closest_obj].transform.element[0][3],
                        SurfaceNormal.y += object[closest_obj].transform.element[1][3],
                        SurfaceNormal.z += object[closest_obj].transform.element[2][3],
-                       SurfaceNormal.w = 1);
+                       SurfaceNormal.w = 1);*/
          if (shadow_ray(rename_me, cur_light_pos, closest_obj) == 0 ){
             ToLight = vector_normalise(vector_subtract(cur_light_pos, SurfaceNormal));
 
