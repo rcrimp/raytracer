e603eb8884da5c4dbde8e5e05d477bb976c1a13a
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 04:40:50 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -231,7 +231,7 @@ RGBColour ray_trace(RayDef ray, int recurse_depth) {
                        SurfaceNormal.z += object[closest_obj].transform.element[2][3],
                        SurfaceNormal.w = 1);*/
          //if (shadow_ray(rename_me, cur_light_pos, closest_obj) == 0 ){
-         if (shadow_ray(SurfaceNormal, light_source[cur_light].position, closest_obj) == 0){
+         if (shadow_ray(rename_me, light_source[cur_light].position, closest_obj) == 0){
          ToLight = vector_normalise(vector_subtract(cur_light_pos, SurfaceNormal));
 
             double nl = vector_dot(SurfaceNormal, ToLight);
