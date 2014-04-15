016d40dbb296820245dae8342e574d2d1003ec8e
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 00:03:42 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -144,7 +144,7 @@ RGBColour ray_trace(RayDef ray, int recurse_depth) {
       cur_ray.start = vector_transform(ray.start, object[closest_obj].transform);
 
       /* transform the ray by the camera */
-      cur_ray.direction = vector_transform(ray.direction, camera.transform);
+      //cur_ray.direction = vector_transform(ray.direction, camera.transform);
       
       /* Lighting calculations */
       Vector SurfacePoint = (vector_add(cur_ray.start, vector_scale(cur_ray.direction, t)));
