b0c9e09bfc06422e4496ae7c9e3106673e287b9e
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 05:53:50 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -300,8 +300,8 @@ void renderImage(void) {
             for(j = 0; j < grid_size; j++){
                //double FOV_jitter = 2*(rand() / (double)RAND_MAX)-1; /* [-1, 1} */
 
-               ray.start.x += 2*(rand() / (double)RAND_MAX)-1;
-               ray.start.y += 2*(rand() / (double)RAND_MAX)-1;
+               //ray.start.x += 2*(rand() / (double)RAND_MAX)-1;
+               //ray.start.y += 2*(rand() / (double)RAND_MAX)-1;
                
                ray.direction.x = -camera.view_size/2 + pixel_size*(col + (double)i/grid_size);
                ray.direction.y = camera.view_size/2 - pixel_size*(row + (double)j/grid_size);
