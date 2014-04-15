eace951057f53ab3903d70df8db468010539c288
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 03:22:10 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -178,7 +178,8 @@ RGBColour ray_trace(RayDef ray, int recurse_depth) {
               cur_ray.direction = vector_transform(ray.direction, object[cur_obj].transform);
              */
          if(closest_obj == 1){  
-            Vector temp_l_pos = vector_transform(cur_light_pos, object[0].transform);
+            Vector temp_l_pos =
+               vector_transform(light_source[cur_light].position, object[0].transform);
 
             SurfaceNormal.w = 1;
             Vector sta = vector_transform(SurfaceNormal, object[1].transform);
