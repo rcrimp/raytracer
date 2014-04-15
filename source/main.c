492795f5792572f1c555b1692f152eaa54d29935
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 01:16:10 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -114,7 +114,7 @@ RGBColour ray_trace(RayDef ray, int recurse_depth) {
       cur_ray.start     = vector_transform(ray.start,     object[cur_obj].transform);
       /* transform the ray direction */
       
-      cur_ray.direction = vector_transform(ray.direction, matrix_transpose(object[cur_obj].transform));
+      cur_ray.direction = vector_transform(ray.direction, object[cur_obj].transform);
       //cur_ray.direction = vector_normalise(cur_ray.direction);
       //printf("%f\n", vector_length(cur_ray.direction));
 
