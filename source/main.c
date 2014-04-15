fb890bae191f12f752f12b7d5729599e767293b4
Reuben Crimp <reubencrimp@gmail.com>
2014-04-16 00:47:27 +1200

    compiled build

--- source/main.c
+++ source/main.c
@@ -58,8 +58,8 @@ RGBColour texture_diffuse(RGBColour diffuse_colour, int texture, Vector surface_
    double su = acos(surface_point.x/tmp)/M_PI*180.0;
    double sv = atan(surface_point.y/tmp)/M_PI*180.0;
 
-   if(abs(cv - sv) < 0.001 )
-      printf(".");
+   if(!abs(cv - sv) < 0.001 )
+      printf("fuck\n");
    
    switch(texture){
    case 0: /* default: don't change diffuse colour at all */
