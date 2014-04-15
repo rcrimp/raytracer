abe532e396373f7e59cf5beacabb5b8da528ee56
Reuben Crimp <reubencrimp@gmail.com>
2014-04-15 17:10:04 +1200

    compiled build

--- source/matrix_ops.c
+++ source/matrix_ops.c
@@ -93,10 +93,10 @@ void matrix_multiply(Matrix *m, Matrix n){
    for (row = 0; row < MATRIX_SIZE; row++) { // for each row of m
       for (col = 0; col < MATRIX_SIZE; col++) { // for each column of n
          m->element[row][col] =
-            m->element[0][col] * n.element[row][0] +
-            m->element[1][col] * n.element[row][1] +
-            m->element[2][col] * n.element[row][2] +
-            m->element[3][col] * n.element[row][3];
+            m->element[0][row] * n.element[col][0] +
+            m->element[1][row] * n.element[col][1] +
+            m->element[2][row] * n.element[col][2] +
+            m->element[3][row] * n.element[col][3];
       }
    }
 }
