1e95ada1d8ae58a1a3d140f2c23d13d5cfce3a76
Reuben Crimp <reubencrimp@gmail.com>
2014-04-15 20:43:05 +1200

    compiled build

--- source/matrix_ops.c
+++ source/matrix_ops.c
@@ -94,20 +94,18 @@ void matrix_scale(Matrix *m, double s){
 }
 
 void matrix_multiply_right(Matrix *A, Matrix B){
-   /* store B*A into A */
+   Matrix *temp;
    int row, col;   
    for (row = 0; row < MATRIX_SIZE; row++) {
       for (col = 0; col < MATRIX_SIZE; col++) {
-         A->element[row][col] =
+         temp->element[row][col] =
             B.element[row][0] * A->element[0][col] +
             B.element[row][1] * A->element[1][col] +
             B.element[row][2] * A->element[2][col] +
             B.element[row][3] * A->element[3][col];
-         printf("A%d%d ", row, col);
-         printf("B%d0 * A0%d ", row, col);
       }
-      printf("\n");
    }
+   A = &temp;
 }
 
 void matrix_multiply_left(Matrix *A, Matrix B){
