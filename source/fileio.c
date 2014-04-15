d7ff74c982aa7b6494ce4889eda1d96c327511c6
Reuben Crimp <reubencrimp@gmail.com>
2014-04-15 18:33:06 +1200

    compiled build

--- source/fileio.c
+++ source/fileio.c
@@ -300,7 +300,7 @@ void fileio_readfile(char *fname) {
          fscanf(description_file, "%lf", &angle);
 
          //angle to rads
-         angle = angle*(M_PI/180);
+         angle = angle*(2*M_PI/180);
          if( num_objs != 0){
             angle = -angle;
          }
